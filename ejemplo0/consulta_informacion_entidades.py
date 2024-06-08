18765d5c5cf76f4c616837c99e76d801ad3bed95
joechuro1206 <joech1206@gmail.com>
2024-06-07 21:11:55 -0500

    ejercicio0-tuplas

--- ejemplo0/consulta_informacion_entidades.py
+++ ejemplo0/consulta_informacion_entidades.py
@@ -12,43 +12,18 @@ cursor = conn.cursor()
 # a través de cursor su puede ejecutar comandos SQL mediante el método
 # execute
 
-# Crear una cadena que almacene la sentencia de ingreso de información
-# se recuerda los atributos: nombre, apellido, cedula, edad
-nombre = "Andrés Vinicio"
-apellido = "Jara Vinces"
-cedula = "1011019091"
-edad = 30
-cadena_sql = """INSERT INTO Autor (nombre, apellido, cedula, edad) \
-VALUES ('%s', '%s', '%s', %d);""" % (nombre, apellido, cedula, edad)
-
-# ejecutar el SQL
-cursor.execute(cadena_sql)
-
-# nuevo registo
-nombre = "Ana Salas"
-cadena_sql = """INSERT INTO Autor (nombre, apellido, cedula, edad) \
-VALUES ('%s', '%s', '%s', %d);""" % (nombre, apellido, cedula, edad)
-cursor.execute(cadena_sql)
-
-# nuevo registo
-nombre = "Marco Barcia"
-cadena_sql = """INSERT INTO Autor (nombre, apellido, cedula, edad) \
-VALUES ('%s', '%s', '%s', %d);""" % (nombre, apellido, cedula, edad)
-cursor.execute(cadena_sql)
-
-# confirmar los cambios a través del objeto importado de tip Connection
-conn.commit()
-
 # hace consultas a la base de datos
 cadena_consulta_sql = "SELECT * from Autor"
 cursor.execute(cadena_consulta_sql)
 # la información resultante se la obtiene del método fetchall de cursor.
 informacion = cursor.fetchall()
-
+print(informacion)
 # se realiza un ciclo repetitivo para recorrer la secuencia de información
 # resultante
 for d in informacion:
-    print("%s - %s - %s - %d" % (d[0], d[1], d[2], d[3]))
+    print("El nombre es: %s y la edad es: %d"% (d[0], d[3]))
+    # Destruimos el for para conocer como se recorre la informacion
 
+    # d va a reccorer informacion que es una lista, dentro tiene una tupla de datos
 # cerrar el enlace a la base de datos (recomendado)
 cursor.close()
